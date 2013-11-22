8f4e1835285b0a46a4302e78d5e753a14ad7ade1
jayunit100 <jayunit100@gmail.com>
2013-11-22 11:28:06 -0500

    fs write test : mavenized w a small test

--- FSWriteTest/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PDIR=`pwd`
-
-CLASSPATH="${PDIR}/lib:${PDIR}/lib/hadoop-core-1.1.2.23.jar:${PDIR}/glusterfs-0.0.1-20130418-1738.jar"
-
-cd src
-javac -d ${PDIR} -classpath ${CLASSPATH} DFSTester.java
-cd $PDIR 
